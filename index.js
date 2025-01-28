2554fccd5b6d2562af618e14b4c40dfe48e9bd7e
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-01-27 22:32:04 -0300

    Gerando 5 cartas

--- index.js
+++ index.js
@@ -119,6 +119,7 @@ document.querySelectorAll('.personagem').forEach(function(objeto) {
     selecionarObjeto(this);
   });
 });
+
 document.querySelectorAll('.bloco, .porta, .blocoDeCombate, .areaDeNado').forEach(function(alvo) {
   alvo.addEventListener('click', function() {
     if (objetoSelecionado) {
@@ -133,52 +134,52 @@ document.querySelectorAll('.bloco, .porta, .blocoDeCombate, .areaDeNado').forEac
 
 ///Gerar Cartas
 
-// Selecione o botão e a div personagem
-const botaoGerarCartas = document.querySelector(".gerarCartas");
-const divPersonagem = document.querySelector(".personagem");
-
-// Adicione um evento de clique ao botão
-botaoGerarCartas.addEventListener("click", () => {
-  // Crie um novo objeto da classe Personagem
-  const personagem = new Personagem('Leonidas','a pé', 'guerreiro', "./images/iLeonidas.jpg");
-  
-  // Crie o elemento HTML para o personagem
-  const elemento = personagem.criarElemento();
-  
-  // Adicione o elemento HTML à div personagem
-  divPersonagem.appendChild(elemento);
-});
-
 
 class Personagem {
-  constructor(nome, locomocao, classe, imgPath) {
+  constructor(nome, locomocao, classe, imgPath, div) {
     this.nome = nome;
     this.locomocao = locomocao;
     this.classe = classe;
     this.imgPath = imgPath;
+    this.div = div;
   }
-  
-  criarElemento() {
-    const div = document.querySelector(".personagem");
-    div.style.cursor = 'pointer'
-    div.style.backgroundImage = `url(${this.imgPath})`;
-    div.style.backgroundSize = 'cover';
-    div.style.width = '32px';
-    div.style.height = '32px';
-    div.style.marginLeft = '1px'
-    div.style.marginTop = '1px'
-    div.style.border = '2px solid rgb(0,14,138)'
-  
-    // Adicione o evento de mouseover
-    div.addEventListener("mouseover", () => {
-      div.style.transform = "scale(1.2)";
-      div.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.5)";
+
+  criarPersonagem() {
+    this.div.style.backgroundImage = `url(${this.imgPath})`;
+    this.div.style.backgroundSize = 'cover';
+    this.div.style.width = '32px';
+    this.div.style.height = '32px';
+    this.div.style.marginLeft = '1px'
+    this.div.style.marginTop = '1px'
+    this.div.style.border = '2px solid rgb(0,14,138)'
+    this.div.style.cursor = 'pointer';
+
+    this.div.addEventListener("mouseover", () => {
+      this.div.style.transform = "scale(1.2)";
+      this.div.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.5)";
     });
-  
-    // Adicione o evento de mouseout
-    div.addEventListener("mouseout", () => {
-      div.style.transform = "scale(1)";
-      div.style.boxShadow = "none";
+
+    this.div.addEventListener("mouseout", () => {
+      this.div.style.transform = "scale(1)";
+      this.div.style.boxShadow = "none";
     });
   }
 }
+
+const botaoGerarCartas = document.querySelector(".gerarCartas");
+const divs = document.querySelectorAll(".personagem");
+
+const personagens = [
+  new Personagem('Leonidas', 'a pé', 'espartano', './images/iLeonidas.jpg', divs[0]),
+  new Personagem('Aquiles', 'a pé', 'guerreiro', './images/iGuerreiro.jpg', divs[1]),
+  new Personagem('Zion', 'a pé', 'arqueiro', './images/iNinja.jpg', divs[2]),
+  new Personagem('Dante', 'a pé', 'berserker', './images/iBerserker.jpg', divs[3]),
+  new Personagem('Xin', 'a pé', 'samurai', './images/iSamurai.jpg', divs[4])
+  
+];
+
+botaoGerarCartas.addEventListener("click", () => {
+  personagens.forEach(personagem => personagem.criarPersonagem());
+});
+
+
